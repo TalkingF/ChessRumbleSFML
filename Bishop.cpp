dadd2f05aee2fba6a2ee4166e28c760d87d5e9a3
Lucas Owers <elpookyvg@gmail.com>
2023-05-24 11:20:49 +0000

    fixed bishop

--- Bishop.cpp
+++ Bishop.cpp
@@ -1,6 +1,7 @@
 #include "Bishop.h"
 #include "Board.h"
 #include "Square.h"
+using namespace std;
 
 Bishop::Bishop(bool colour_, int row_, int col_) : Piece(colour_, row_, col_) {}
 
@@ -8,27 +9,25 @@ bool Bishop::legal_move(int new_row, int new_col, const Board& board) {
     if (!Piece::legal_move(new_row, new_col, board)) {
         return false;
     }
-
+    int row_diff = new_row - row;
+    int col_diff = new_col - col;
     const vector<vector<Square>>& boardData = board.get_board();
-    const Square& currentSquare = boardData[row][col];
 
-    int row_diff = new_row - get_row();
-    int col_diff = new_col - get_col();
-    if (abs(row_diff) != abs(col_diff)) {
-        return false;
-    }
-
-    int row_step = (row_diff > 0) ? 1 : -1;
-    int col_step = (col_diff > 0) ? 1 : -1;
-    int row = get_row() + row_step;
-    int col = get_col() + col_step;
-
-    while (row != new_row && col != new_col) {
-        if (currentSquare.get_piece() != nullptr) {
-            return false;
+    if (abs(row_diff) == abs(col_diff)) {
+        // Check for obstacles diagonally
+        int row_step = row_diff > 0 ? 1 : -1;
+        int col_step = col_diff > 0 ? 1 : -1;
+        int r = row + row_step;
+        int c = col + col_step;
+        while (r != new_row && c != new_col) {
+            if (boardData[r][c].get_piece() != nullptr) {
+                return false;
+            }
+            r += row_step;
+            c += col_step;
         }
-        row += row_step;
-        col += col_step;
+    } else {
+        return false;
     }
 
     return true;
